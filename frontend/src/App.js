c20dc21981fcd82e33467429bd7d6a5426cf151a
jenkins.scott@palmcoastd.com <jenkins.scott@palmcoastdata.com>
2021-01-10 18:05:02 -0500

    chapter9

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
-import data from './data';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="grid-container">
     <header className="row">
         <div>
             <a className="brand" href="/">CDB Shipped 2 U</a>
-            test
         </div>
         <div>
             <a href="/cart">Cart</a>
@@ -14,38 +17,14 @@ function App() {
         </div>
     </header>
     <main> 
-        <div className="row center">
-            {
-              data.products.map((product) => 
-                <div key={product._id} className="card">
-                <a href={`/product/${product._id}`}>
-                    <img className="medium" src={product.image} alt={product.name} />
-                </a>
-                <div className="card-body">
-                    <a href={`/product/${product._id}`}>
-                        <h2>{product.name}</h2>
-                    </a>
-                    <div className="rating">
-                        <span><i className="fa fa-star-o"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                    </div>
-                    <div className="price">${product.price}</div>
-                </div>
-            </div>
-                
-                
-                )
-            }
-            
-        </div>
+        <Route path="/product/:id" component={ProductScreen} ></Route>
+        <Route path="/" component={HomeScreen} exact></Route>
     </main>
     <footer className="row center">
         All Rights Reserved
     </footer>
 </div>
+</BrowserRouter>
 
   );
 }
